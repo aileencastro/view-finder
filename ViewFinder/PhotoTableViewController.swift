7482055e355ed1e9271236a7edb6966b0ac2eed5
aileencastro <aileencastrogg@gmail.com>
2019-07-16 14:19:50 -0700

    Complete Iteration 4

--- ViewFinder/PhotoTableViewController.swift
+++ ViewFinder/PhotoTableViewController.swift
@@ -27,17 +27,17 @@ class PhotoTableViewController: UITableViewController {
         super.viewDidLoad()
 
         // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+//         self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+//         self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        
         return photos.count
     }
 
@@ -54,9 +54,6 @@ class PhotoTableViewController: UITableViewController {
                 cell.imageView?.image = cellPhotoImage
             }
         }
-        
-//        //configure the cell
-//        cell.imageView?.image = UIImage(named: "harry")
 
         return cell
     }
@@ -82,49 +79,32 @@ class PhotoTableViewController: UITableViewController {
         }
     }
    
-    /*
-    // Override to support conditional editing of the table view.
+    
+//     Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
+//         Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+ 
 
-    /*
-    // Override to support editing the table view.
+    
+//     Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+//             Delete the row from the data source
+           if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+           {
+            //now that we have access to Core Data we need to select the row, then delete the photo
+            //we also need to save the context and reload
+            let photoToDelete = photos[indexPath.row]
+            //now delete, then save, then reload
+            context.delete(photoToDelete)
+            //save the context, then reload
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+            getPhotos()
+            }
+        }
+
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
